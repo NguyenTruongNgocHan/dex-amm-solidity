37ac308dead515fc7af2fcf7c798bd1f638788c8
NguyenTruongNgocHan <23520434@gm.uit.edu.vn>
2026-04-25 14:57:41 +0700

    test: validate full DEX flow (swap, liquidity, events) end-to-end

--- scripts/interact.js
+++ scripts/interact.js
@@ -5,9 +5,9 @@ async function main() {
 
   const [user] = await ethers.getSigners();
 
-  const ammAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-  const tokenAAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const tokenBAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  const ammAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+  const tokenAAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+  const tokenBAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 
   const amm = await ethers.getContractAt("SimpleAMM", ammAddress);
   const tokenA = await ethers.getContractAt("MockERC20", tokenAAddress);
